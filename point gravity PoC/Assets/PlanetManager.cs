71063aae25daeb459f901e7dbd0bd0abaf2d6e4b
kravening <sep_ul_tura@live.nl>
2016-02-09 16:18:42 +0100

    proof of concept for point gravity

---
+++ point gravity PoC/Assets/PlanetManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+public static class PlanetManager
+{
+	static List<Planet> planets = new List<Planet> ();
+
+	public static void Register (Planet item)
+	{
+		if (!planets.Contains (item)) {
+			planets.Add (item);
+		}
+	}
+
+	public static void UnRegister (Planet item)
+	{
+		if (planets.Contains (item)) {
+			planets.Remove (item);
+		}
+	}
+
+	public static Planet[] GetPlanets ()
+	{
+		return planets.ToArray ();
+	}
+}
---
+++ point gravity PoC/Assets/PointGravity.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PointGravity : MonoBehaviour
+{
+	[TooltipAttribute("The minimum amount of pull needed to affect the object")]
+	public float
+		minPull = 0f;
+	Rigidbody rb;
+	// Use this for initialization
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+		if (rb.useGravity == true) {
+			rb.useGravity = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		Planet[] items = PlanetManager.GetPlanets ();
+		foreach (Planet planet in items) {
+			float pullRadius = planet.GetPullRadius();
+			//calculate distance between object and planets
+			float distance = Vector3.Distance (planet.transform.position, transform.position);
+			if (distance <= pullRadius) {
+
+				//get the radius of the planet
+				float radius = planet.GetAvgRadius ();
+				// Calculate the force needed based on the object's distance, gravity and radius
+				// Formula: https://en.wikipedia.org/wiki/Gravity_of_Earth#Altitude
+				float acceleration = planet.standardAcceleration * Mathf.Pow ((radius / (radius + distance)), 2f);
+				if (acceleration > minPull) {
+					//apply force to the object
+					rb.AddForce ((planet.transform.position - transform.position).normalized * acceleration);
+				}
+			}
+		}
+	}
+}
